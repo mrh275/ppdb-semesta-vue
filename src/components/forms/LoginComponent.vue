cdfa046796151afb63e3e6b4cfde86ed28bbf32a
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-17 21:05:20 +0700

    create component for login form

---
+++ src/components/forms/LoginComponent.vue
@@ -0,0 +1,82 @@
+<script>
+export default {
+  name: "LoginComponent",
+};
+</script>
+<template>
+  <div
+    class="fixed top-0 z-50 invisible w-full transition-all duration-300 ease-in-out opacity-0 login-form"
+    id="login-form"
+  >
+    <div
+      class="flex items-center justify-center h-screen py-6 bg-opacity-50 bg-zinc-700"
+    >
+      <div class="px-4 mx-auto max-w-screen-2xl md:px-8">
+        <form
+          method="POST"
+          id="login-form"
+          class="max-w-lg mx-auto overflow-hidden bg-white border rounded-lg"
+        >
+          <div
+            class="relative flex items-center justify-center p-4 bg-gray-100"
+          >
+            <h2
+              class="text-2xl font-bold text-center text-gray-800 lg:text-3xl"
+            >
+              Login PPDB
+            </h2>
+            <button
+              type="button"
+              id="close-login-btn"
+              class="close-login-form absolute text-2xl text-zinc-600 cursor-pointer font-bold top-0.5 right-2"
+            >
+              <i class="fa-solid fa-xmark"></i>
+            </button>
+          </div>
+          <div class="flex flex-col gap-4 px-8 pt-4 pb-8">
+            <div>
+              <label
+                for="email"
+                class="inline-block mb-2 text-sm text-gray-800 sm:text-base"
+                >Username</label
+              >
+              <input
+                name="username"
+                id="username"
+                class="w-full bg-gray-50 text-gray-800 border focus:ring ring-[#0099ff] rounded outline-none transition duration-100 px-3 py-2"
+              />
+            </div>
+
+            <div>
+              <label
+                for="password"
+                class="inline-block mb-2 text-sm text-gray-800 sm:text-base"
+                >Password</label
+              >
+              <input
+                name="password"
+                type="password"
+                id="password"
+                class="w-full bg-gray-50 text-gray-800 border focus:ring ring-[#0099ff] rounded outline-none transition duration-100 px-3 py-2"
+              />
+            </div>
+
+            <div>
+              <input type="checkbox" name="showPassword" id="showPassword" />
+              <label for="showPassword">Show Password</label>
+            </div>
+
+            <button
+              class="block btn btn-primary"
+              type="button"
+              onclick="loginAttempt()"
+              id="login-btn-form"
+            >
+              Log in
+            </button>
+          </div>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
