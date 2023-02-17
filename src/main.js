82a2ff76aa15c0dfd4261cee69b28a579ed81085
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-17 23:48:56 +0700

    moved unused line

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
-
 import "./assets/main.css";
+import axios from "axios";
 
+axios.defaults.withCredentials = true;
+axios.defaults.baseURL = 'http://127.0.0.1:8000/api/'
 const app = createApp(App);
 
 app.use(createPinia());
