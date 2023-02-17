c351718a6af006c44a0675b6f44d55165112e968
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-02-17 21:05:10 +0700

    rename component

---
+++ src/components/layout/NavbarComponent.vue
@@ -0,0 +1,66 @@
+<template>
+  <nav class="navbar">
+    <div class="brand">
+      <img src="/assets/img/LOGO-XS.png" alt="Logo Sekolah" class="brand-img" />
+      <a href="/" class="brand-title">PPDB SMAN 1 RAWAMERTA</a>
+    </div>
+    <div class="menu-wrapper">
+      <ul class="nav-menu">
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">Beranda</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">Daftar</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">Persyaratan</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">Kuota dan Jalur</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">Data Pendaftar</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">FAQ</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/">Kontak</RouterLink>
+        </li>
+        <li class="nav-item">
+          <RouterLink class="nav-link" to="/" @click.prevent="showLoginForm"
+            >Login</RouterLink
+          >
+        </li>
+      </ul>
+      <div class="toggle-darkMode" id="toggle-darkMode">
+        <span class="flex w-4 overflow-x-hidden darkMode-btn">
+          <i class="text-white bx bxs-moon"></i>
+          <i class="text-yellow-600 bx bxs-sun"></i>
+        </span>
+      </div>
+    </div>
+    <div class="toggle-menu">
+      <span class="burger"></span>
+      <span class="burger"></span>
+      <span class="burger"></span>
+    </div>
+  </nav>
+</template>
+
+<script>
+import LoginComponent from "../forms/LoginComponent.vue";
+export default {
+  methods: {
+    showLoginForm() {
+      const lastMenu = document.querySelector(".nav-menu").lastElementChild;
+      console.log(lastMenu);
+      document.querySelector("#login-form").classList.remove("invisible");
+      document.querySelector("#login-form").classList.remove("opacity-0");
+    },
+    components: {
+      LoginComponent,
+    },
+  },
+};
+</script>
