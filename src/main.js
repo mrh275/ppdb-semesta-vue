a58fcb7ead512c3def85cdd7d73795cd82c6368b
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-10-05 11:53:36 +0700

    import and use primevue

--- src/main.js
+++ src/main.js
@@ -1,9 +1,16 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import PrimeVue from "primevue/config";
+import DataTable from "primevue/datatable";
+import Column from "primevue/column";
+import ColumnGroup from 'primevue/columngroup';   // optional
+import Row from 'primevue/row';
 
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
+import "primevue/resources/themes/bootstrap4-light-blue/theme.css";
+// import "primevue/resources/themes/lara-dark-blue/theme.css";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
@@ -12,5 +19,10 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(PrimeVue, { ripple: true });
+app.component("DataTable", DataTable);
+app.component("Column", Column);
+app.component("ColumnGroup", ColumnGroup);
+app.component("Row", Row);
 
 app.mount("#app");
