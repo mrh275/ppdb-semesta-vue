a4f74d6493308356c339b4775cce4009211ec7df
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2023-10-07 21:14:38 +0700

    remove prime libraries plugin

--- src/main.js
+++ src/main.js
@@ -1,20 +1,9 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
-import PrimeVue from "primevue/config";
-import DataTable from "primevue/datatable";
-import Column from "primevue/column";
-import ColumnGroup from 'primevue/columngroup';   // optional
-import Row from 'primevue/row';
-import InputText from "primevue/inputtext";
-import Paginator from 'primevue/paginator';
 
 import App from "./App.vue";
 import router from "./router";
 import "./assets/main.css";
-import "primeflex/primeflex.css";
-import "primevue/resources/themes/lara-light-blue/theme.css";
-import "primevue/resources/primevue.min.css"; /* Deprecated */
-import "primeicons/primeicons.css";
 import axios from "axios";
 
 axios.defaults.withCredentials = true;
@@ -23,12 +12,5 @@ const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
-app.use(PrimeVue);
-app.component("DataTable", DataTable);
-app.component("Column", Column);
-app.component("ColumnGroup", ColumnGroup);
-app.component("Row", Row);
-app.component("InputText", InputText);
-app.component("Paginator", Paginator);
 
 app.mount("#app");
--- src/views/DataPendaftar.vue
+++ src/views/DataPendaftar.vue
@@ -4,74 +4,25 @@
   >
     <div class="relative px-6 py-8 bg-white shadow-md rounded-xl">
       <h2 class="data-pendaftar-title">Data Pendaftar</h2>
-      <div class="card">
-        <DataTable
-          :value="siswa"
-          v-model:filters="filters"
-          paginator
-          :rows="10"
-          :rowsPerPageOptions="[5, 10, 20, 50]"
-          :globalFilterFields="['nama', 'noreg_ppdb', 'nisn', 'tempat_lahir']"
-          dataKey="id"
-          stripedRows
-          tableStyle="min-width: 50rem"
-        >
-          <template #header>
-            <div class="flex justify-content-end">
-              <span class="p-input-icon-left">
-                <i class="pi pi-search" />
-                <InputText
-                  class="py-2 pl-5 pr-2 border-gray-400 border-1"
-                  v-model="filters['global'].value"
-                  placeholder="Keyword Search"
-                />
-              </span>
-            </div>
-          </template>
-          <template #empty> No customers found. </template>
-          <template #loading> Loading customers data. Please wait. </template>
-          <Column field="noreg_ppdb" sortable header="No Pendaftaran"></Column>
-          <Column field="nisn" sortable header="NISN"></Column>
-          <Column field="nama" sortable header="Nama"></Column>
-          <Column
-            field="tempat_lahir"
-            sortable
-            header="Tempat Tanggal Lahir"
-          ></Column>
-          <template #footer>
-            In total there are {{ siswa ? siswa.length : 0 }} products.
-          </template>
-        </DataTable>
-      </div>
+      <div class="card"></div>
     </div>
   </div>
 </template>
 
 <script>
 import axios from "axios";
-import { FilterMatchMode } from "primevue/api";
 
 export default {
   name: "DataPendaftar",
   data() {
     return {
       siswa: [],
-      filters: {
-        global: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        nama: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        noreg_ppdb: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        nisn: { value: null, matchMode: FilterMatchMode.CONTAINS },
-        tempat_lahir: { value: null, matchMode: FilterMatchMode.CONTAINS },
-      },
     };
   },
   mounted() {
     const API_url = "http://127.0.0.1:8000/api/data-pendaftar";
     axios
       .request({
-        headers: {
-          Authorization: `Bearer 1|SSawcSZK4a6WnpwCCL1PKg2FABu7ayYmRmsJfR70`,
-        },
         method: "GET",
         url: API_url,
       })
