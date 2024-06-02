80ca00e8093eedaab890c6d2345637773f57e9ec
Muhamad Ramdani Hidayatullah <muhamadramdani.h275@gmail.com>
2024-06-02 23:45:57 +0700

    modified emit method for search query

--- src/components/forms/SearchFormTable.vue
+++ src/components/forms/SearchFormTable.vue
@@ -1,8 +1,10 @@
-<script setup>
-const emit = defineEmits(["search"]);
-
-const search = (e) => {
-  emit("search", e.target.value);
+<script>
+export default {
+  methods: {
+    search(event) {
+      this.$emit("search", event.target.value);
+    },
+  },
 };
 </script>
 <template>
@@ -11,6 +13,6 @@ const search = (e) => {
     @input="search"
     id="table-search"
     class="block p-2 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-    placeholder="Search for items"
+    placeholder="Cari nama siswa"
   />
 </template>
